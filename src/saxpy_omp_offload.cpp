902c72865f1fb5b399f4a658002d4c6e7ef8df5a
Ralf Mueller <ralf.mueller@dkrz.de>
2019-05-16 15:08:34 +0200

    add gpu offloading version

---
+++ src/saxpy_omp_offload.cpp
@@ -0,0 +1,30 @@
+#include "saxpy.h"
+#include <omp.h>
+
+int main() {
+   float *x = new float[N], *y = new float[N];
+
+   int g_num_threads = omp_get_num_threads();
+#pragma omptarget teams distribute parallel for
+   for (int i = 0; i < N; ++i) {
+      x[i] = XVAL;
+      y[i] = YVAL;
+   }
+   std::cout << "Number of threads: " << g_num_threads << std::endl;
+   std::cout << "N: " << N << std::endl;
+
+   saxpy_timer timer;
+
+#pragma omptarget teams distribute parallel for
+      for (int i=0; i<N; ++i)
+         y[i] += AVAL * x[i];
+
+   auto elapsed = timer.elapsed_msec();
+   std::cout << "Elapsed: " << elapsed << " ms\n";
+
+   saxpy_verify(y);
+   delete[] x;
+   delete[] y;
+   return 0;
+}
+
